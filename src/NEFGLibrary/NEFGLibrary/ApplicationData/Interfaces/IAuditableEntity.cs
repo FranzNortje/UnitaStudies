1b9f012658e4ded80f127886aa9c132abb94c5f7
Unknown <lnortje@gmail.com>
2018-08-06 23:59:31 +0200

    Adding classes for DB and UI

---
+++ src/NEFGLibrary/NEFGLibrary/ApplicationData/Interfaces/IAuditableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEFGLibrary.CompanyData.Interfaces
+{
+    public interface IAuditableEntity
+    {
+        string CreatedBy { get; set; }
+        string UpdatedBy { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ src/NEFGLibrary/NEFGLibrary/HttpUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using AspNet.Security.OpenIdConnect.Primitives;
+
+namespace NEFGLibrary
+{
+    public class HttpUnitOfWork : UnitOfWork
+    {
+        public HttpUnitOfWork(ApplicationDbContext context, IHttpContextAccessor httpAccessor) : base(context)
+        {
+            context.CurrentUserId = httpAccessor.HttpContext.User.FindFirst(OpenIdConnectConstants.Claims.Subject)?.Value?.Trim();
+        }
+    }
+}
