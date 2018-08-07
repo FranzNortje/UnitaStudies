330fd0c146c383ae7e056457a8afe3b563a8088b
Franz Friedrich Johann Nortje <franznortje.studies@gmail.com>
2018-08-07 22:50:55 +0200

    Created Growth Percentage gathering method Need help with DataProjection.ModifyGrowthPercentage Need a Math.Power that works with decimals

---
+++ src/NEFGLibrary/NEFGLibrary/CompanyData/DataProjection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace NEFGLibrary.CompanyData
+{
+    public class DataProjection
+    {
+        //=(C2/12)-((1+C2)^(1/12)-1)+C2
+        public static decimal ModifyGrowthPercentage(int portfolioId,int growthDataId,MorningStar ms)
+        {
+            //Growth Perc Value
+            var value = MorningStar.GetSpecificGrowthDataValue(portfolioId, growthDataId, ms);
+            return (value / 12) - (DecimalPower((1 + value),(value / 12)) - 1) + value;
+            // ((0.065)/12)-(POWER((1+(0.065)),(1/12))-1)+(0.065)
+        }
+
+        // need to figure this out
+        //https://www.extremeoptimization.com/Documentation/Reference/Extreme.Mathematics.DecimalMath.Pow-1.aspx
+        // method below ineffective - returns inaccurate result
+        private static decimal DecimalPower(decimal x, decimal y)
+        {
+            Double X = (double)x;
+            Double Y = (double)y;
+            return (decimal)System.Math.Pow(X, Y);
+        }
+    }
+}
--- src/NEFGLibrary/NEFGLibrary/CompanyData/MorningStar.cs
+++ src/NEFGLibrary/NEFGLibrary/CompanyData/MorningStar.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using NEFGLibrary.Helper;
 using NEFGLibrary.Investment.Interfaces;
+using System.Linq;
 
 namespace NEFGLibrary.CompanyData
 {
   public class MorningStar : IValidator
   {
+      
+
     public int MorningStarVersion { get; set; }
     public DateTime DirectDataDateAssetAllocation { get; set; }
     public DateTime DirectDataDatePerformances { get; set; }
@@ -19,5 +22,12 @@ namespace NEFGLibrary.CompanyData
     {
       return new List<ErrorResponse>();
     }
-  }
+
+
+        public static decimal GetSpecificGrowthDataValue(int portfolioId, int growthDataId, MorningStar ms)
+        {
+            string check = $"P{portfolioId}:G{growthDataId}";
+            return ms.PortfolioGrowthDataInfo.First(pg => pg.GrowthdataId == check).MonthBaseValue;
+        }
+    }
 }
